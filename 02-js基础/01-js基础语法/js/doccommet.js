b1c1f28318e23a0fa38b464cd4005df149ef6b9f
Pyhita <pyhita.dev@gmail.com>
2023-08-02 20:52:11 +0800

    js基础 变量 数据类型

---
+++ 02-js基础/01-js基础语法/js/doccommet.js
@@ -0,0 +1,9 @@
+
+/**
+ * 测试函数
+ * @param {string} name 姓名 
+ */
+function test(name) {
+
+}
+
---
+++ 02-js基础/01-js基础语法/js/outer.js
@@ -0,0 +1,4 @@
+const ele = document.querySelector('.btn2');
+ele.onclick = function() {
+  alert("点我弹窗了！")
+}
