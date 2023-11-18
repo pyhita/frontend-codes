8360fce55bdb61b038153b6cb00ac09a599c0606
pyhita <pyhita.dev@gmail.com>
2023-11-18 20:59:28 +0800

    vue 脚手架开发

---
+++ 06-Vue3实战/11_vue脚手架/01_product_demo/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <h2>{{ msg }}</h2>
+
+    <product-item>
+        
+    </product-item>
+</template>
+
+<script>
+import ProductItem from './ProductsItem.vue'
+export default {
+    components: {
+        "product-item": ProductItem
+    },
+
+    data() {
+        return {
+            msg: '我是标题'
+        }
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ 06-Vue3实战/11_vue脚手架/01_product_demo/src/ProductsItem.vue
@@ -0,0 +1,12 @@
+<template>
+    <h2>我是产品</h2>
+    <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        msg: "组件化开发"
+    },
+}
+</script>
